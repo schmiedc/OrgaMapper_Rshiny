8a2bc6449db91e6c1b3e25183dc40911648924b7
Christopher Schmied <christopher.schmied@gmail.com>
2020-10-22 18:44:50 +0200

    First test of GUI with rendered plots

--- app.R
+++ app.R
@@ -2,8 +2,12 @@ setwd("/data1/FMP_Docs/Repositories/plugins_FMP/orgaMapper_R/")
 library(gridExtra)
 library(shiny)
 library(shinyFiles)
+library("openxlsx")
+library(gridExtra)
 
-# source("orgaShiny.R")
+source("process_data.R")
+source("plot_data.R")
+
 # ============================================================================
 #
 #  DESCRIPTION: Data analysis for OrgaMapper
@@ -142,10 +146,8 @@ ui <- fluidPage(
                   
                   tabsetPanel(type = "tabs",
                               # takes outputId plot
-                              tabPanel("Cell Measurements", plotOutput("plot", height=1200)),
-                              # takes outputId finalTable
-                              tabPanel("Table", dataTableOutput(outputId = 'finalTable'))
-                              
+                              tabPanel("Cell Measurements", plotOutput("cell_plots", height=1200)),
+                              tabPanel("Organelle Measurements", plotOutput("orga_plots", height=1200))
                   )
                   
                 )
@@ -193,48 +195,131 @@ server <- function(input, output, session) {
   
   observeEvent(input$processData, {
 
-    inputDirectory <- global$datapath
+    # ==========================================================================
+    # Params
     
+    # path to folder where the directories for the measurements are
+    directory = "/home/schmiedc/Desktop/Test/test_nd2/2020-10-14_output/"
+    
+    result_name = "Analysis_test"
+    
+    # filter for feret's diameter
+    feret_lower = 0
+    feret_upper = 600
+    
+    # determine range for plots
+    norm_distance_nucleus = 0.7
+    
+    # TODO if file contains series number or the already present column
+    # needs to default to something sensible if not possible
+    single_series = FALSE
+    series_regex = "(?<=_)\\d*($)"
+    
+    # TODO apply background subtraction for plots
+    plot_background_subtract = TRUE
+    
+    # analyze signal profiles
+    analyze_signal_profiles = TRUE
+    
+    # Binning for intensity profiles
+    # or different method for binning
+    upper_limit_norm = 1
+    bin_width_norm = 0.05
+    
+    upper_limit = 75
+    bin_width = 2
+    
+    # ==========================================================================
+    # where to save the data
+    out_dir =  directory
+    
+    # result_path <- file.path(out_dir, result_name, fsep = .Platform$file.sep)
+    
+    # plot dir
+    
+    plots_dir <- file.path(out_dir, plots, fsep = .Platform$file.sep)
+    
+    dir.create(plots_dir, showWarnings = FALSE)
+    
+    # ==========================================================================
     name_distance = "organelleDistance.csv"
     name_cell_measure = "cellMeasurements.csv"
     
-    organelle_distance <- read_collected_files(name_distance, )
-    cell_measure <- read_collected_files(name_cell_measure, )
-
-    orga_column = ncol(organelle_distance);
-    cell_column = ncol(cell_measure);
+    organelle_distance <- read_collected_files(directory, 
+                                               name_distance, 
+                                               single_series, 
+                                               series_regex)
     
     
+    cell_measure <- read_collected_files(directory, 
+                                         name_cell_measure, 
+                                         single_series, 
+                                         series_regex)
     
-    # collects raw data
-    table.signal <- collectList(inputDirectory, labelSignal, input$timeRes)
-    table.background <- collectList(inputDirectory, labelBackground, input$timeRes)
+    orga_column = ncol(organelle_distance)
+    cell_column = ncol(cell_measure)
     
-    # calculates average mean intensity per frame
-    avg.signal <- calcMean(table.signal)
-    avg.background <- calcMean(table.background)
+    cell_measure_filter <- process_cell_measurements(cell_measure, 
+                                                     feret_lower, 
+                                                     feret_upper,
+                                                     cell_column,
+                                                     orga_column)
+    
+    merge_cell_organelle <- process_orga_measurements(cell_measure_filter,
+                                                      organelle_distance,
+                                                      cell_column,
+                                                      orga_column)
+    
+    merged_summary <- create_summary_table(merge_cell_organelle,
+                                           cell_measure_filter)
+    
+    # --------------------------------------------------------------------------
+    # save processed data
+    write.xlsx(file = paste0( result_path, "_detection.xlsx", sep = ""), 
+               merge_cell_organelle, 
+               sheetName="Sheet1",  
+               col.names=TRUE, 
+               row.names=TRUE, 
+               append=FALSE, 
+               showNA=TRUE)
+    
+    # merged_summary
+    write.xlsx(file = paste0( result_path,  "_cell.xlsx", sep = ""), 
+               merged_summary, 
+               sheetName="Sheet1",  
+               col.names=TRUE, 
+               row.names=TRUE, 
+               append=FALSE, 
+               showNA=TRUE)
+    
+    # --------------------------------------------------------------------------
+    # plot data
+    cell_plots <- plot_cell_measurements(cell_measure_filter,
+                                         plots_dir,
+                                         cell_column,
+                                         orga_column)
+    
+    detection_plots <- plot_detection_measurements(merge_cell_organelle,
+                                                   merged_summary,
+                                                   plots_dir,
+                                                   cell_column,
+                                                   orga_column)
     
     
-    
-    
-    
-    # generate final table
-    finalTable <- processData(indir, input$frameStim, avg.signal, avg.background)
-    
-    output$finalTable <- renderDataTable({
+   
+    output$cell_plots <- renderPlot({
       
-      finalTable
+      cell_plots <- do.call(grid.arrange, plot_list_cell)
+      
+      print(cell_plots)
       
     })
     
-    plot.list <- plotMeans(avg.signal, avg.background, finalTable)
-    
-    
-    output$plot <- renderPlot({
+    output$orga_plots <- renderPlot({
       
-      plots <- grid.arrange(grobs = plot.list, ncol = 1, top = "Processing results")
+      orga_plots <- do.call(grid.arrange, plot_list_detection)
       
-      print(plots)
+      print(orga_plots)
       
     })
     
